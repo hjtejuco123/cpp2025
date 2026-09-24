c7001c72fbfc6c162208ace58221b995a141ea03
Hadji Tejuco <hadji.tejuco@gmail.com>
2026-09-25 07:57:42 +0800

    Create sample3.cpp

---
+++ module1/sample3.cpp
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+variable names
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+    //var declare
+    int studentAge;
+    double studentGrade;
+    
+    cout << "Student Information\n\n";
+    cout << "Enter age: ";
+    cin >> studentAge;
+    
+    cout << "Enter grade: ";
+    cin >> studentGrade;
+    
+    cout << "\nAge: " << studentAge<<endl;
+    cout << "Grade: "<<studentGrade<<endl;
+    
+    cout<<"Hello World";
+
+    return 0;
+}
