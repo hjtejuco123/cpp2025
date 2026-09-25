fa092d9611d954c864aeb05f4a7be0e5762dbbf3
Hadji Tejuco <hadji.tejuco@gmail.com>
2026-09-25 08:22:20 +0800

    Create sample5.cpp

---
+++ module1/sample5.cpp
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+char input
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char section;
+    
+    cout<<"Enter your section Letter: ";
+    cin >> section;
+
+    cout << "Your section is " << section;
+    return 0;
+}
