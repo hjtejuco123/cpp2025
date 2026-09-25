89498964cd654292040a1d530ea01efb29b6b7e7
Hadji Tejuco <hadji.tejuco@gmail.com>
2026-09-25 08:38:38 +0800

    Add sample7.cpp for basic variable initialization

---
+++ module1/sample7.cpp
@@ -0,0 +1,23 @@
+/******************************************************************************
+Initialization
+
+age 18 
+grade  90.5
+section A 
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age = 18;  
+    double grade = 90.5;
+    char section = 'A';
+    
+    cout << "Age: " << age <<endl;
+    cout << "Grade: " << grade <<endl;
+    cout << "Section: "<< section << endl;
+
+    return 0;
+}
