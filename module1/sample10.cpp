9ac4b786f4f0a267098a6ccc9657878115636d75
Hadji Tejuco <hadji.tejuco@gmail.com>
2026-09-25 09:14:50 +0800

    Create sample10.cpp

---
+++ module1/sample10.cpp
@@ -0,0 +1,29 @@
+/******************************************************************************
+p       q          p && q       p || q          !p    
+
+                    FAND         TOR        T   F     
+                                            F   T  
+T       T           T            T 
+T       F           F            T     
+F       T           F            T     
+F       F           F            F 
+
+0 - false 
+1 - true 
+
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cout << "Enter first number: ";
+    cin >> a;
+    cout << "Enter second number: ";
+    cin >> b; 
+    cout << boolalpha;
+    cout << "Are they equal: " << (a == b); // 1 0 
+
+    return 0;
+}
